602e79075cd2ba8c3472a296d4d349a4578396b6
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-03-01 14:22:16 -0500

    ffhw shift

--- plot.py
+++ plot.py
@@ -41,26 +41,19 @@ if __name__ == "__main__":
     test_loader = dm.test_dataloader()
 
     with torch.no_grad():
-        rcGAN_model = rcGAN.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/rcgan/checkpoint_best.ckpt')
-
-        rcGAN_model.cuda()
-
-        rcGAN_model.eval()
-
-        eigenGAN_model = EigenGAN.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/eigengan/checkpoint_best.ckpt')
-
-        eigenGAN_model.cuda()
-
-        eigenGAN_model.eval()
-
-        coModGAN_model = CoModGAN.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/comodgan/checkpoint_best.ckpt')
-
-        coModGAN_model.cuda()
-
-        coModGAN_model.eval()
+        if args.eigengan:
+            method = 'eigengan'
+            model = EigenGAN.load_from_checkpoint(
+                checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint_best.ckpt')
+        elif args.comodgan:
+            method = 'comodgan'
+            model = CoModGAN.load_from_checkpoint(
+                checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint_best.ckpt')
+        else:
+            model = rcGAN.load_from_checkpoint(
+                checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint_best.ckpt')
+        model.cuda()
+        model.eval()
 
         for i, data in enumerate(test_loader):
             y, x, mask, mean, std = data[0]
@@ -70,43 +63,25 @@ if __name__ == "__main__":
             mean = mean.cuda()
             std = std.cuda()
 
-            gens_rcgan = torch.zeros(
-                size=(y.size(0), cfg.num_z_test, 3, cfg.im_size, cfg.im_size)).cuda()
-            gens_eigengan = torch.zeros(
-                size=(y.size(0), cfg.num_z_test, 3, cfg.im_size, cfg.im_size)).cuda()
-            gens_comodgan = torch.zeros(
+            gens = torch.zeros(
                 size=(y.size(0), cfg.num_z_test, 3, cfg.im_size, cfg.im_size)).cuda()
 
             for z in range(cfg.num_z_test):
-                gens_rcgan[:, z, :, :, :] = rcGAN_model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
-                gens_eigengan[:, z, :, :, :] = eigenGAN_model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
-                gens_comodgan[:, z, :, :, :] = coModGAN_model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
+                gens[:, z, :, :, :] = model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
 
             gt = x * std[:, :, None, None] + mean[:, :, None, None]
             zfr = y * std[:, :, None, None] + mean[:, :, None, None]
 
             for j in range(y.size(0)):
-                np_samps = {
-                    'comodgan': [],
-                    'rcgan': [],
-                    'eigengan': []
-                }
-
                 np_gt = None
 
                 np_gt = gt[j].cpu().numpy()
                 np_zfr = zfr[j].cpu().numpy()
 
-                for z in range(cfg.num_z_test):
-                    np_samps['comodgan'].append(gens_comodgan[j, z].cpu().numpy())
-                    np_samps['rcgan'].append(gens_rcgan[j, z].cpu().numpy())
-                    np_samps['eigengan'].append(gens_eigengan[j, z].cpu().numpy())
-
-
-                methods = ['comodgan', 'rcgan', 'eigengan']
+                np_samps = gens[j].cpu().numpy()
 
                 # Global recon, error, std
-                nrow = len(methods)
+                nrow = 1
                 ncol = 7
 
                 fig = plt.figure(figsize=(ncol + 1, nrow + 1))
@@ -132,17 +107,16 @@ if __name__ == "__main__":
                 ax.set_yticks([])
                 ax.set_title("y")
 
-                for k in range(len(methods)):
-                    for l in range(5):
-                        ax = plt.subplot(gs[k, l + 2])
-                        im = ax.imshow(np.transpose(np_samps[methods[k]][l], (1, 2, 0)))
-                        ax.set_xticklabels([])
-                        ax.set_yticklabels([])
-                        ax.set_xticks([])
-                        ax.set_yticks([])
-                        # ax.set_title(f"{methods[k]} {l+1}")
+                for l in range(5):
+                    ax = plt.subplot(gs[0, l + 2])
+                    im = ax.imshow(np.transpose(np_samps[l], (1, 2, 0)))
+                    ax.set_xticklabels([])
+                    ax.set_yticklabels([])
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+                    # ax.set_title(f"{methods[k]} {l+1}")
 
-                plt.savefig(f'figures/inpainting/example_{fig_count}.png', bbox_inches='tight', dpi=300)
+                plt.savefig(f'figures/inpainting/example_{method}_{fig_count}.png', bbox_inches='tight', dpi=300)
                 plt.close(fig)
 
                 if fig_count == args.num_figs:
--- plot_e_vecs.py
+++ plot_e_vecs.py
@@ -41,26 +41,19 @@ if __name__ == "__main__":
     test_loader = dm.test_dataloader()
 
     with torch.no_grad():
-        rcGAN_model = rcGAN.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/rcgan/checkpoint_best.ckpt')
-
-        rcGAN_model.cuda()
-
-        rcGAN_model.eval()
-
-        eigenGAN_model = EigenGAN.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/eigengan/checkpoint_best.ckpt')
-
-        eigenGAN_model.cuda()
-
-        eigenGAN_model.eval()
-
-        coModGAN_model = CoModGAN.load_from_checkpoint(
-            checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/comodgan/checkpoint_best.ckpt')
-
-        coModGAN_model.cuda()
-
-        coModGAN_model.eval()
+        if args.eigengan:
+            method = 'eigengan'
+            model = EigenGAN.load_from_checkpoint(
+                checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint_best.ckpt')
+        elif args.comodgan:
+            method = 'comodgan'
+            model = CoModGAN.load_from_checkpoint(
+                checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint_best.ckpt')
+        else:
+            model = rcGAN.load_from_checkpoint(
+                checkpoint_path=cfg.checkpoint_dir + args.exp_name + '/checkpoint_best.ckpt')
+        model.cuda()
+        model.eval()
 
         for i, data in enumerate(test_loader):
             y, x, mask, mean, std = data[0]
@@ -70,17 +63,11 @@ if __name__ == "__main__":
             mean = mean.cuda()
             std = std.cuda()
 
-            gens_rcgan = torch.zeros(
-                size=(y.size(0), cfg.num_z_test, 3, cfg.im_size, cfg.im_size)).cuda()
-            gens_eigengan = torch.zeros(
-                size=(y.size(0), cfg.num_z_test, 3, cfg.im_size, cfg.im_size)).cuda()
-            gens_comodgan = torch.zeros(
+            gens = torch.zeros(
                 size=(y.size(0), cfg.num_z_test, 3, cfg.im_size, cfg.im_size)).cuda()
 
             for z in range(cfg.num_z_test):
-                gens_rcgan[:, z, :, :, :] = rcGAN_model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
-                gens_eigengan[:, z, :, :, :] = eigenGAN_model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
-                gens_comodgan[:, z, :, :, :] = coModGAN_model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
+                gens[:, z, :, :, :] = model.forward(y, mask) * std[:, :, None, None] + mean[:, :, None, None]
 
             gt = x * std[:, :, None, None] + mean[:, :, None, None]
             zfr = y * std[:, :, None, None] + mean[:, :, None, None]
@@ -100,33 +87,17 @@ if __name__ == "__main__":
                 # TODO: Get evecs
 
                 for z in range(cfg.num_z_test):
-                    np_samps['comodgan'].append(gens_comodgan[j, z].cpu().numpy())
-                    np_samps['rcgan'].append(gens_rcgan[j, z].cpu().numpy())
-                    np_samps['eigengan'].append(gens_eigengan[j, z].cpu().numpy())
+                    np_samps = gens[j].cpu().numpy()
 
-                cov_mat_rcgan = np.zeros((20, 3 * np_gt.shape[-1] * np_gt.shape[-2]))
-                cov_mat_eigengan = np.zeros((20, 3 * np_gt.shape[-1] * np_gt.shape[-2]))
-                cov_mat_comodgan = np.zeros((20, 3 * np_gt.shape[-1] * np_gt.shape[-2]))
+                cov_mat = np.zeros((20, 3 * np_gt.shape[-1] * np_gt.shape[-2]))
 
                 for z in range(20):
-                    cov_mat_rcgan[z, :] = np_samps['rcgan'][z].flatten()
-                    cov_mat_eigengan[z, :] = np_samps['eigengan'][z].flatten()
-                    cov_mat_comodgan[z, :] = np_samps['comodgan'][z].flatten()
+                    cov_mat[z, :] = np_samps[z].flatten()
 
-                _, _, vh_rcgan = np.linalg.svd(cov_mat_rcgan, full_matrices=False)
-                _, _, vh_eigengan = np.linalg.svd(cov_mat_eigengan, full_matrices=False)
-                _, _, vh_comodgan = np.linalg.svd(cov_mat_comodgan, full_matrices=False)
+                _, _, vh = np.linalg.svd(cov_mat, full_matrices=False)
 
-                np_vh = {
-                    'comodgan': vh_comodgan,
-                    'rcgan': vh_rcgan,
-                    'eigengan': vh_eigengan
-                }
-                methods = ['comodgan', 'rcgan', 'eigengan']
-
-                # Global recon, error, std
-                nrow = len(methods)
-                ncol = 2
+                nrow = 1
+                ncol = 5
 
                 fig = plt.figure(figsize=(ncol + 1, nrow + 1))
 
@@ -136,18 +107,17 @@ if __name__ == "__main__":
                                        left=0.5 / (ncol + 1), right=1 - 0.5 / (ncol + 1))
 
 
-                for k in range(len(methods)):
-                    for l in range(2):
-                        ax = plt.subplot(gs[k, l ])
-                        im_np = np_vh[methods[k]][l].reshape((3, 256, 256))
-                        im_np = (im_np - np.min(im_np)) / (np.max(im_np) - np.min(im_np))
-                        im = ax.imshow(im_np.transpose(1, 2, 0))
-                        ax.set_xticklabels([])
-                        ax.set_yticklabels([])
-                        ax.set_xticks([])
-                        ax.set_yticks([])
+                for l in range(5):
+                    ax = plt.subplot(gs[0, l ])
+                    im_np = vh[l].reshape((3, 256, 256))
+                    im_np = (im_np - np.min(im_np)) / (np.max(im_np) - np.min(im_np))
+                    im = ax.imshow(im_np.transpose(1, 2, 0))
+                    ax.set_xticklabels([])
+                    ax.set_yticklabels([])
+                    ax.set_xticks([])
+                    ax.set_yticks([])
 
-                plt.savefig(f'figures/inpainting_evecs/example_{fig_count}.png', bbox_inches='tight', dpi=300)
+                plt.savefig(f'figures/inpainting_evecs/example_{method}_{fig_count}.png', bbox_inches='tight', dpi=300)
                 plt.close(fig)
 
                 if fig_count == args.num_figs:
