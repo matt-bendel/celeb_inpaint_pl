7f11e028bd4e462bb9bcaf631ad1493a0cab1df2
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-03-05 14:50:11 -0500

    ffhw shift

--- plot_e_vecs.py
+++ plot_e_vecs.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
 
                 for l in range(5):
                     ax = plt.subplot(gs[0, l])
-                    im_np = vh[l + 1].reshape((3, 256, 256))
+                    im_np = vh[l].reshape((3, 256, 256))
                     im_np = (im_np - np.min(im_np)) / (np.max(im_np) - np.min(im_np))
                     im = ax.imshow(im_np.transpose(1, 2, 0))
                     ax.set_xticklabels([])
