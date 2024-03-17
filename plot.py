0d528335dddbcde426e540a8950a8b5e691cd758
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-03-17 19:11:18 -0400

    ffhw shift

--- plot.py
+++ plot.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
                     running_count += 1
                     continue
 
-                if running_count not in [623]:
+                if running_count not in [633]:
                     running_count += 1
                     continue
 
@@ -115,7 +115,7 @@ if __name__ == "__main__":
                                        top=1. - 0.5 / (nrow + 1), bottom=0.5 / (nrow + 1),
                                        left=0.5 / (ncol + 1), right=1 - 0.5 / (ncol + 1))
 
-                inds = [18, 8, 5, 7, 16]
+                inds = [28, 21, 19, 11, 4]
                 for l in range(5):
                     ax = plt.subplot(gs[0, l])
                     im = ax.imshow(np.transpose(np_samps[inds[l]], (1, 2, 0)))
