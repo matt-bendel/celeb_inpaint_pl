37c241b0481095526d040b1f08f2f8ea486a389b
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-08-05 11:36:10 -0400

    Added Ohayon

--- plot_rebuttal.py
+++ plot_rebuttal.py
@@ -60,10 +60,10 @@ if __name__ == "__main__":
             mean = mean.cuda()
             std = std.cuda()
 
-            # mask = torch.zeros(mask.shape)
-            # for j in range(x.shape[0]):
-            #     mask[j] = torch.load(f'/storage/matt_models/inpainting/dps/test_20k/image_{count + j}_mask.pt')
-            #
+            mask = torch.zeros(mask.shape)
+            for j in range(x.shape[0]):
+                mask[j] = torch.load(f'/storage/matt_models/inpainting/dps/test_20k/image_{count + j}_mask.pt')
+
             mask = mask.cuda()
 
             count += y.shape[0]
