bd16319666509fb933d3250a5b121a76064c7b7c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-10-07 14:28:04 -0400

    Added ohayon to plot

--- plot_diffusion_rebuttal.py
+++ plot_diffusion_rebuttal.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
                 for j in range(x.shape[0]):
                     for k in range(5):
                         gens[j, k] = torch.load(f'/storage/matt_models/inpainting/{diff_model}/test_20k/image_{count + j}_sample_{k}.pt') * std[j, :, None, None].cpu() + mean[j, :, None, None].cpu()
-                    mask[j] = torch.load(f'/storage/matt_models/inpainting/{diff_model}/test/image_{count + j}_mask.pt')
+                    mask[j] = torch.load(f'/storage/matt_models/inpainting/dps/test/image_{count + j}_mask.pt')
 
                 gens = gens.cuda()
                 mask = mask.cuda()
